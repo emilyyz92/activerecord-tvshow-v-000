32da284d55ef4bc628ea7a23bac2b569facf256c
Emily Zhao <tong.zhao.92@gmail.com>
2018-04-27 19:45:40 +0000

    automatically backed up by learn

--- app/models/shows.rb
+++ app/models/shows.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.lowest_rating
-    Show.where("rating = 10").last.rating
+    Show.where("rating = 2").last.rating
   end
 
   def self.most_popular_show
