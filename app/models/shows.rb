983f79a9811de6206f9194c9dbb076629e769201
Emily Zhao <tong.zhao.92@gmail.com>
2018-04-27 18:40:42 +0000

    automatically backed up by learn

--- app/models/shows.rb
+++ app/models/shows.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
   def self.highest_rating
     binding.pry
     a = Show.where("rating = 10").first
-    a.rating
+    a.first.rating
   end
 
   def self.most_popular_show
